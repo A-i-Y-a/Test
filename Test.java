e9bff23ac7156d212ac71e0e23e5445b861b58f4
Allen <Allen@Mains-MacBook-Pro.local>
2018-12-10 16:48:44 -0500

    Update for Robot

--- Test.java
+++ Test.java
@@ -1,8 +1,58 @@
-class Test{
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        for(int i = 1; i < 1000; ++i) {
-            System.out.println(i);
+import edu.wpi.cscore.AxisCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.*;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class Robot extends IterativeRobot {
+
+    WPI_TalonSRX FrontRight;
+    WPI_TalonSRX FrontLeft;
+    WPI_TalonSRX BackRight;
+    WPI_TalonSRX BackLeft;
+    SpeedControllerGroup Right;
+    SpeedControllerGroup Left;
+    DifferentialDrive DriveTrain;
+    XboxController Controller;
+
+    void RobotInit {
+        FrontRight = new WPI_TalonSRX(2);
+        FrontLeft = new WPI_TalonSRX(3);
+        BackRight = new WPI_TalonSRX(4);
+        BackLeft = new WPI_TalonSRX(5);
+
+        Right = new SpeedControllerGroup(FrontRight, BackRight);
+        Left = new SpeedControllerGroup(FrontLeft, BackLeft);
+
+        DriveTrain = new DifferentrialDrive(Left, Right);
+
+        Controller = new XboxController(1);
+    }
+
+    void teleopInit {
+        // Nothing
+    }
+
+    void teleopPeriodic {
+        
+        if(Controller.getAButton()) {
+            double y = Controller.getY(GenericHID.Hand.kLeft);
+            double x = Controller.getX(GenericHID.Hand.kLeft);
+            DriveTrain.arcadeDrive(y, x);
+        } else if(Controller.getBumper(GenericHID.Hand.kLeft)) {
+            double y = Controller.getY(GenericHID.Hand.kLeft);
+            double x = Controller.getX(GenericHID.Hand.kRight);
+            DriveTrain.arcadeDrive(y, x);
+        } else if(Controller.getBumper(GenericHID.Hand.kRight)) {
+            double y = Controller.getX(GenericHID.Hand.kRight);
+            double x = Controller.getY(GenericHID.Hand.kLeft);
+            DriveTrain.arcadeDrive(y, x);
+        } else {
+            // Nothing
         }
     }
 }
